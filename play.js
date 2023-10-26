8b59e5e22c2f366e0741cb57e86111908c1eb740
Rachel Little <racheldawnlittle@gmail.com>
2023-10-26 15:27:59 -0600

    functioning snake

--- play.js
+++ play.js
@@ -1,28 +1,7 @@
-const net = require("net");
 const { connect } = require("./client");
-
-// setup interface to handle user input from stdin
-const setupInput = function() {
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding("utf8");
-    stdin.resume();
-    stdin.on("data", handleUserInput);
-    return stdin;
-  };
-
-const handleUserInput = function(key) {
-    process.stdin.on("data", (key) => {
-        process.stdout.write(".")
-    })
-    if (key === '\u0003') {
-        process.exit();
-    }
-
-};
-
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
-setupInput();
+const connection = connect();
+setupInput(connection);
 
