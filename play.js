d574adf70e15d7a291607ef31d0dc0d0dc757d80
Rachel Little <racheldawnlittle@gmail.com>
2023-10-25 20:10:36 -0600

    module connection

--- play.js
+++ play.js
@@ -1,20 +1,4 @@
 const net = require("net");
+const { connect } = require("./client");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243', // IP address here,
-    port: 50541 // PORT number here,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log("Received data:", data);
-  });
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
