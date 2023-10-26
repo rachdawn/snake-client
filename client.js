7794e8e82f6971066b8bad0e9dca4f9ce5c514c2
Rachel Little <racheldawnlittle@gmail.com>
2023-10-26 00:03:47 -0600

    I hate snakes

--- client.js
+++ client.js
@@ -1,23 +1,28 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function() {
-    const conn = net.createConnection({
-      host: '165.227.47.243', // IP address here,
-      port: 50541 // PORT number here,
-    });
-  
-    // interpret incoming data as text
-    conn.setEncoding("utf8");
-  
-    conn.on("data", (data) => {
-      console.log("Received data:", data);
-    });
-    return conn;
-  };
-  
-  console.log("Connecting ...");
-  connect();
-  
-  module.exports = { connect };
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243", // IP address here,
+    port: 50541, // PORT number here,
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
 
+  conn.on("data", (data) => {
+    console.log("Received data:", data);
+  });
+  // print when successfully connected
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server!");
+    conn.write("name: RDL");});
+  
+  setTimeout(() => {
+    conn.write("Move: up");
+  }, 3000);
+
+  return conn;
+};
+
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,4 +1,13 @@
 const net = require("net");
 const { connect } = require("./client");
 
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    return stdin;
+  };
 
+console.log("Connecting ...");
+connect();
