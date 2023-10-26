cb18090e11251bfe3db0b19208e9eb8a2ec5c3ef
Rachel Little <racheldawnlittle@gmail.com>
2023-10-26 14:38:37 -0600

    exit function

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "165.227.47.243", // IP address here,
     port: 50541, // PORT number here,
@@ -21,12 +21,16 @@ const connect = function () {
     console.log("Received data:", data);
   });  
 
-  setTimeout(() => {
+  /*setTimeout(() => {
     conn.write("Move: up");
-  }, 3000);
+  }, 3000);*/
 
   return conn;
 };
 
+/* "Move: up" - move up one square (unless facing down)
+"Move: down" - move down one square (unless facing up)
+"Move: left" - move left one square (unless facing right)
+"Move: right" - move left one square (unless facing left) */
 
 module.exports = { connect };
--- play.js
+++ play.js
@@ -1,13 +1,28 @@
 const net = require("net");
 const { connect } = require("./client");
 
-const setupInput = function () {
+// setup interface to handle user input from stdin
+const setupInput = function() {
     const stdin = process.stdin;
     stdin.setRawMode(true);
     stdin.setEncoding("utf8");
     stdin.resume();
+    stdin.on("data", handleUserInput);
     return stdin;
   };
 
+const handleUserInput = function(key) {
+    process.stdin.on("data", (key) => {
+        process.stdout.write(".")
+    })
+    if (key === '\u0003') {
+        process.exit();
+    }
+
+};
+
+
 console.log("Connecting ...");
 connect();
+setupInput();
+
